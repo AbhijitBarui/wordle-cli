38dfc0cc78ca1bcd90d6fafedb6515081b5ccb54
AbhijitBarui <abhijitongit@gmail.com>
2022-04-29 13:56:34 +0530

    final final commit

---
+++ utils.py
@@ -0,0 +1,60 @@
+## important modules ##
+
+
+#help text
+help_text = '''
+                        :Welcome to Wordle:
+The rules are simple
+You have 6 attempts, using those you have to guess the 5 letter word by entering valid 5 letter words
+to help you guess there are 3 highlights:
+highlight 1: 🇼 hen the letter is in the word and in right position
+highlight 2: 🅷 🆆 en the letter is in the word but not in position
+highlight 3: 🄿 hen the letter is not in the word
+highlight 4: when its not even a letter
+type help to see this message again but remember it takes away a valuable attempt!!
+warning: inputs below lettercount of 5, invalid words or invalid characters are counted as attempt!!
+lets begin!!
+'''
+
+#dictionary loader function
+def load_dict(file_name):
+    file = open(file_name)
+    words = file.readlines()
+    file.close()
+    return [word[:5].upper() for word in words]
+
+
+#highlighter function
+def highlighter(word, dict_h):
+    for letter in word:
+        if letter in dict_h.keys():
+            for key, value in dict_h.items():
+                if letter.lower() == key: return value ;break
+        else: return "❓"
+
+
+#wordleChecker function
+def wordleChecker(answer, sample, h1, h2, h3):
+    arrans = list(answer.lower())
+    arrsam = list(sample.lower())
+    wordle_return=[]
+    wordle_word = ""
+    for idx1, ltr1 in enumerate(arrsam):
+        for idx2,ltr2 in enumerate(arrans):
+            if ltr1 == ltr2: #H1/H2 filter
+                if idx1 == idx2: #check_3_1: H1 filter
+                    wordle_return.append(highlighter(ltr1, h1))
+                    arrans.pop(idx2)
+                    arrans.insert(idx2,'%')
+                    break
+                else:
+                    wordle_return.append(highlighter(ltr1, h2))
+                    break
+            if ltr1 not in arrans:
+                wordle_return.append(highlighter(ltr1, h3))
+                break
+    even_count = 0
+    for wordle_letters in wordle_return:
+        wordle_word+=str(wordle_letters) + "   "
+
+    return wordle_word
--- wordle.py
+++ wordle.py
@@ -1,84 +1,31 @@
-from multiprocessing.dummy import Array
 import random
 from highlights import h1, h2, h3
+from utils import help_text, load_dict, highlighter, wordleChecker
 
-print('''
-                        :Welcome to Wordle:
-The rules are simple
-You have 6 attempts, using those you have to guess the 5 letter word by entering valid 5 letter words
-to help you guess there are 3 highlights:
-highlight 1: 🇼 hen the letter is in the word and in right position
-highlight 2: 🅷 🆆 en the letter is in the word but not in position
-highlight 3: 🄿 hen the letter is not in the word
-highlight 4: when its not even a letter
-lets begin!!
-''')
+#introductory text
+print(help_text)
 
-def load_dict(file_name):
-    file = open(file_name)
-    words = file.readlines()
-    file.close()
-    return [word[:5].upper() for word in words]
-
+#loading arrays for word checking and answer selecting
 words_dict = load_dict("wordle_words.txt")
+words_checker = load_dict("wordle_words.txt")
 word_answer = random.choice(words_dict)
 
-wordleflag = False
+#attempt count
 attempt_count = 6
 
-def highlighter(word, dict_h):
-    for letter in word:
-        if letter in dict_h.keys():
-            for key, value in dict_h.items():
-                if letter.lower() == key: return value ;break
-        else: return "❓"
-
-def wordleChecker(answer, sample):
-    arrans = list(answer.lower())
-    arrsam = list(sample.lower())
-    wordle_return=[]
-    wordle_word = ""
-    for idx1, ltr1 in enumerate(arrsam):
-        for idx2,ltr2 in enumerate(arrans):
-            if ltr1 == ltr2: #H1/H2 filter
-                if idx1 == idx2: #check_3_1: H1 filter
-                    wordle_return.append(highlighter(ltr1, h1))
-                    arrans.pop(idx2)
-                    arrans.insert(idx2,'%')
-                    break
-                else:
-                    wordle_return.append(highlighter(ltr1, h2))
-                    break
-            if ltr1 not in arrans:
-                wordle_return.append(highlighter(ltr1, h3))
-                break
-    even_count = 0
-    for wordle_letters in wordle_return:
-        wordle_word+=str(wordle_letters) + "   "
-
-    return wordle_word
-
+#attempt iterations
 for attempt in range(attempt_count):
-    sample = input(f'attempts left {6 - attempt}, enter the word: ')
-    if sample.upper()==word_answer:
-        print("correct guess")
+    sample = input(f'attempts left {6 - attempt}, enter the word: ') #taking input
+    if sample.upper()==word_answer: #check to see whether we got it in first try
+        print(wordleChecker(word_answer, sample, h1, h2, h3))
+        print("correct guess, you WON!! Thanks for playing")
         break
-    if len(sample) == 5:
-        #check 1 check of same words: whether the words are same
-        print(wordleChecker(word_answer, sample))
-    elif sample.lower() == 'help':
-        print('''
-The rules are simple
-You have 6 attempts, using those you have to guess the 5 letter word by entering valid 5 letter words
-to help you guess there are 3 highlights:
-highlight 1: 🇼 hen the letter is in the word and in right position
-highlight 2: 🅷 🆆 en the letter is in the word but not in position
-highlight 3: 🄿 hen the letter is not in the word
-highlight 4: when its not even a letter
-lets begin: 
-''')
+    if len(sample) == 5: #check to see whether the entered string is of length 5
+        print(wordleChecker(word_answer, sample, h1, h2, h3))
+    elif sample.lower() == 'help': #help command
+        print(help_text)
     else:
         print("less than 5 letters")
-    if attempt == 5:
-            wordleflag = True
-            print(f'answer is {word_answer}')
+
+#print answer in the end
+print(f'answer is {word_answer}')
